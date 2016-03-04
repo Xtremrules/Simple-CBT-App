ab4c5a2d1d3c8adf9ca7301571230aacafe0fbc5
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-04 13:23:12 +0100

    Removed unused usings

--- CBT.Entities/Abstracts/INamedAuditableEntity.cs
+++ CBT.Entities/Abstracts/INamedAuditableEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CBT.Entities.Abstracts
+﻿namespace CBT.Entities.Abstracts
 {
     public interface INamedAuditableEntity : IAuditableEntity
     {
--- CBT.Entities/Setting.cs
+++ CBT.Entities/Setting.cs
@@ -1,9 +1,4 @@
 ﻿using CBT.Entities.Abstracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CBT.Entities
 {
