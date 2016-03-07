72c6eb4f4de37784f5ec71f3fde4789cd5de6900
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-07 16:04:12 +0100

    Added role manager

---
+++ CBT.Domain/Identity/AppRoleManager.cs
@@ -0,0 +1,22 @@
+﻿using CBT.Domain.Concrete;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBT.Domain.Identity
+{
+    public class AppRoleManager : RoleManager<IdentityRole>
+    {
+        public AppRoleManager(RoleStore<IdentityRole> store) : base(store) { }
+        public static AppRoleManager Create(IdentityFactoryOptions<AppRoleManager> options, IOwinContext context)
+        {
+            return new AppRoleManager(new RoleStore<IdentityRole>(context.Get<CBTDbContext>()));
+        }
+    }
+}
