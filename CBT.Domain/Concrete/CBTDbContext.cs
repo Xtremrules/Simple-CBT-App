c272f49f91792e9394d6ebb780132906ad986e84
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-07 17:05:04 +0100

    added DbSet items

--- CBT.Domain/Concrete/CBTDbContext.cs
+++ CBT.Domain/Concrete/CBTDbContext.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Data.Entity;
 using System.Collections.Generic;
+using CBT.Domain.Entities;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace CBT.Domain.Concrete
 {
@@ -15,6 +17,25 @@ namespace CBT.Domain.Concrete
         static CBTDbContext()
         {
             Database.SetInitializer(new CBTDbInitializer());
+        }
+
+        public DbSet<Batch> Batches { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<SQuestion> SQuestions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+
+            modelBuilder.Entity<IdentityUser>().ToTable("Users").Property(x => x.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims").Property(x => x.Id).HasColumnName("UserClaimId");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles").Property(x => x.Id).HasColumnName("RoleName");
         }
     }
 }
