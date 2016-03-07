e49a7c9da83ee3fabdc609bf29073800ad6410e4
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-07 16:54:04 +0100

    Added bundle class

---
+++ CBT.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Optimization;
+using System.Linq;
+using System.Web;
+
+namespace CBT.WebUI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/*.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
