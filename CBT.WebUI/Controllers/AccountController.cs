55efdd0c0ee58ddc1d82ad263eb46bf49a7994aa
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-08 14:08:31 +0100

    Added account controller

---
+++ CBT.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using CBT.Domain.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBT.WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public async Task<ActionResult> Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await UserManager.FindByNameAsync(User.Identity.Name);
+                if (user != null)
+                    return RedirectToAction("Index", "CBT");
+            }
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(string userName, string Password)
+        {
+            var user = await UserManager.FindAsync(userName, Password);
+            if(user == null)
+            {
+                ModelState.AddModelError("", "Username or password incorrect");
+                return View();
+            }
+            AuthManager.SignIn(await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie));
+            return RedirectToAction("Index", "CBT");
+        }
+
+        AppUserManager UserManager
+        {
+            get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); }
+        }
+
+        IAuthenticationManager AuthManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+    }
+}
