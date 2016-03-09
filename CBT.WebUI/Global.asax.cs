b3902921a55f8c9ff969bf0c84c775f3a4d665d2
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-09 05:01:41 +0100

    Added autofac api dependency resolver

--- CBT.WebUI/Global.asax.cs
+++ CBT.WebUI/Global.asax.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Web;
+﻿using CBT.WebUI.Modules;
+using System;
+using Autofac;
+using Autofac.Integration.WebApi;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web;
 
 namespace CBT.WebUI
 {
@@ -15,7 +18,15 @@ namespace CBT.WebUI
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var builder = new Autofac.ContainerBuilder();
+            builder.RegisterApiControllers(typeof(Global).Assembly).PropertiesAutowired();
+            builder.RegisterModule(new ServiceModule());
+            builder.RegisterModule(new EFModule());
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacWebApiDependencyResolver(container));
         }
     }
 }
