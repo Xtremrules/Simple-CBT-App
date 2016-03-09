279c1323f009c760be006d5c2932d11e977fde6e
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-09 14:20:22 +0100

    Added SettingsViewModel

---
+++ CBT.WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CBT.Domain.Abstracts.Services;
+using CBT.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace CBT.WebUI.Controllers
+{
+    [RoutePrefix("api/settings")]
+    public class SettingsController : ApiController
+    {
+        ISettingService _settingsService;
+        public SettingsController(ISettingService _settingsService)
+        {
+            this._settingsService = _settingsService;
+        }
+        // GET: api/Settings
+        public IEnumerable<Setting> Get()
+        {
+            return _settingsService.GetAll();
+        }
+
+        [Route("details/{id:int}")]
+        public async Task<Setting> Get(int id)
+        {
+            return await _settingsService.GetByIdAsync(id);
+        }
+
+        // POST: api/Settings
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Settings/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Settings/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
