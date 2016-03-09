27d686dcc1c98b18ae3d26c73709358ae301c92d
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-09 14:43:58 +0100

    Added more items to view settings viewmodel

--- CBT.WebUI/Models/SettingsViewModel.cs
+++ CBT.WebUI/Models/SettingsViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CBT.WebUI.Models
 {
     public class SettingsViewModel
     {
+        public bool SortQuestion { get; set; }
+        public bool SortOptions { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
