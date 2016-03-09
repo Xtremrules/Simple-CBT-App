70ada22fbb9edc8bd697c8ed0310673d597117e3
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-09 04:55:56 +0100

    Added Service Modules

---
+++ CBT.WebUI/Modules/EFModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using CBT.Domain.Concrete;
+
+namespace CBT.WebUI.Modules
+{
+    public class EFModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(CBTDbContext)).AsSelf();
+        }
+    }
+}
---
+++ CBT.WebUI/Modules/ServiceModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System.Reflection;
+
+namespace CBT.WebUI.Modules
+{
+    public class ServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("CTB.Domain"))
+                .Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+        }
+    }
+}
