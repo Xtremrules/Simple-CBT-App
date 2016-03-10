3683fe5851412e99951360797066752701bafbc6
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-10 13:36:07 +0100

    Added basic api methods

--- CBT.WebUI/Controllers/SettingsController.cs
+++ CBT.WebUI/Controllers/SettingsController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using CBT.Domain.Abstracts.Services;
+﻿using CBT.Domain.Abstracts.Services;
 using CBT.Domain.Entities;
+using CBT.WebUI.Models;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace CBT.WebUI.Controllers
 {
+    [Authorize(Roles = "AdminRole")]
     [RoutePrefix("api/settings")]
     public class SettingsController : ApiController
     {
@@ -31,18 +30,42 @@ namespace CBT.WebUI.Controllers
         }
 
         // POST: api/Settings
-        public void Post([FromBody]string value)
+        public async Task<IHttpActionResult> Post([FromBody]SettingsViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var settings = AutoMapper.Mapper.Map<Setting>(model);
+                await _settingsService.CreateAsync(settings);
+                return Ok();
+            }
+            return BadRequest(ModelState);
         }
 
         // PUT: api/Settings/5
-        public void Put(int id, [FromBody]string value)
+        public async Task<IHttpActionResult> Put(int id, [FromBody]SettingsViewModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var settings = AutoMapper.Mapper.Map<Setting>(model);
+            settings.ID = id;
+            await _settingsService.UpdateAsync(settings);
+            settings = await _settingsService.GetByIdAsync(id);
+            return Ok(settings);
+
         }
 
         // DELETE: api/Settings/5
         public void Delete(int id)
         {
+            throw new NotImplementedException();
         }
     }
 }
