d926e535b25195b870da433ebd79148717043d2a
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-10 15:50:22 +0100

    Added Batch controller

---
+++ CBT.WebUI/Controllers/BatchsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CBT.Domain.Entities;
+using CBT.Domain.Abstracts.Services;
+using System.Threading.Tasks;
+
+namespace CBT.WebUI.Controllers
+{
+    [Authorize(Roles = "AdminRole")]
+    [RoutePrefix("api/batches")]
+    public class BatchsController : ApiController
+    {
+        IBatchService _batchService;
+        public BatchsController(IBatchService _batchService)
+        {
+            this._batchService = _batchService;
+        }
+        // GET: api/Batchs
+        public IEnumerable<Batch> Get()
+        {
+            return  _batchService.GetAll();
+        }
+
+        // GET: api/Batchs/5
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            return Ok(await _batchService.GetBatchbyId(id));
+        }
+
+        // POST: api/Batchs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Batchs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Batchs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
