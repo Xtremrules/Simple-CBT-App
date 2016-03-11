863c3b8d9cd5acb3c2566b58fc231b0d82d40e18
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-11 16:22:21 +0100

    Added Angularjs actions

--- CBT.WebUI/Scripts/CBTApp/home/rootCtrl.js
+++ CBT.WebUI/Scripts/CBTApp/home/rootCtrl.js
@@ -1,4 +1,17 @@
 ﻿angular.module("CBTApp")
-    .controller("rootCtrl", function ($scope, $http) {
-
-});
+    .constant("batchesUrl", "/api/batches")
+    .controller("rootCtrl", function ($scope, $http, batchesUrl) {
+        $scope.data = {};
+        $http.get(batchesUrl).success(function (data) {
+            var d = angular.toJson(data);
+            if (d.length > 0) {
+                $scope.data.empty = true;
+            }
+            else {
+                $scope.data.batches = angular.fromJson(d);
+                //$scope.j = 1;
+            }
+        }).error(function (error) {
+            $scope.data.error = error;
+        });
+    });
