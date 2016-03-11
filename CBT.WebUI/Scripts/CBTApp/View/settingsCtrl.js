288ec3ff849e6a61688a39e7f17b55b16f60d43b
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-11 18:54:12 +0100

    Added settings view

---
+++ CBT.WebUI/Scripts/CBTApp/View/settingsCtrl.js
@@ -0,0 +1,18 @@
+﻿angular.module("CBTApp")
+.constant("settingsUrl", "/api/settings")
+.controller("settingsCtrl", function ($http, $scope, settingsUrl) {
+    $scope.data.empty = null;
+    $http.get(settingsUrl)
+    .success(function (data) {
+        if (data.length <= 0) {
+            
+            $scope.data.empty = true;
+        }
+        else {
+            $scope.data.settings = data;
+        }
+    })
+    .error(function (error) {
+        $scope.error = error;
+    });
+});
--- CBT.WebUI/Scripts/_references.js
+++ CBT.WebUI/Scripts/_references.js
@@ -2,5 +2,6 @@
 /// <reference path="angular.js" />
 /// <reference path="cbtapp/app.js" />
 /// <reference path="cbtapp/view/rootctrl.js" />
+/// <reference path="cbtapp/view/settingsctrl.js" />
 /// <reference path="Resources/angular-resource.js" />
 /// <reference path="Resources/angular-route.js" />
