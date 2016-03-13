323c21c2687353d42d112e3b821a52f20e7cc0b9
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-13 21:15:03 +0100

    Added AddSettings to settingCtrl.js

--- CBT.WebUI/Scripts/CBTApp/Controller/settingsCtrl.js
+++ CBT.WebUI/Scripts/CBTApp/Controller/settingsCtrl.js
@@ -1,11 +1,11 @@
 ﻿angular.module("CBTApp")
 .constant("settingsUrl", "/api/settings")
-.controller("settingsCtrl", function ($http, $scope, settingsUrl) {
+.controller("settingsCtrl", function ($http, $scope, settingsUrl, $location) {
     $scope.data.empty = null;
     $http.get(settingsUrl)
     .success(function (data) {
         if (data.length <= 0) {
-            
+
             $scope.data.empty = true;
         }
         else {
@@ -16,4 +16,17 @@
     .error(function (error) {
         $scope.error = error;
     });
+
+    $scope.Addsettings = function (AddSettingsmodel) {
+        var settings = angular.copy(AddSettingsmodel);
+        $http.post(settingsUrl, settings).success(function (data) {
+
+        })
+        .error(function (error) {
+            $scope.error = error;
+        }).finally(function () {
+            $scope.data.AddSettings = null;
+            $location.path("/settings");
+        });
+    }
 });
