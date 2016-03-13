6e14a4879a877922708490d5937107f3bcef95da
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-13 19:32:30 +0100

    Added Annotation

--- CBT.WebUI/Models/SettingsViewModel.cs
+++ CBT.WebUI/Models/SettingsViewModel.cs
@@ -4,7 +4,9 @@ namespace CBT.WebUI.Models
 {
     public class SettingsViewModel
     {
+        [Required]
         public bool SortQuestion { get; set; }
+        [Required]
         public bool SortOptions { get; set; }
         [Required]
         public string Name { get; set; }
