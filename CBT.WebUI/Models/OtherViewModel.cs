4ff11ee2881abcd805f29020570a591587fbfb02
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-14 15:04:11 +0100

    Added more view model

---
+++ CBT.WebUI/Models/OtherViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CBT.WebUI.Models
+{
+    public class OtherViewModel
+    {
+        [Required]
+        public int OtherID { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
