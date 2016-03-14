40f84e180788e421577b34cd6a63ea8effa4f552
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-14 14:46:27 +0100

    Auto sets default values

--- CBT.Domain/Entities/Setting.cs
+++ CBT.Domain/Entities/Setting.cs
@@ -4,6 +4,11 @@ namespace CBT.Domain.Entities
 {
     public class Setting : NamedAuditableEntity<int>
     {
+        public Setting()
+        {
+            SortOptions = false;
+            SortQuestion = false;
+        }
         public bool SortQuestion { get; set; }
         public bool SortOptions { get; set; }
     }
