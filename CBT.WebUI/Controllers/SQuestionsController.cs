2d724e2ba6aa44fff6e6397eb64ad2fdae2db4fe
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-15 05:31:48 +0100

    apicontroller dev in progress

--- CBT.WebUI/Controllers/SQuestionsController.cs
+++ CBT.WebUI/Controllers/SQuestionsController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using CBT.Domain.Abstracts.Services;
+using CBT.WebUI.Models;
+using AutoMapper;
+using CBT.Domain.Entities;
 using System.Web.Http;
 
 namespace CBT.WebUI.Controllers
@@ -12,20 +17,53 @@ namespace CBT.WebUI.Controllers
     public class SQuestionsController : ApiController
     {
         // GET: api/SQuestions
-        public IEnumerable<string> Get()
+        private ISQuestionService _squestionService;
+        private IQuestionService _questionService;
+        private IOptionService _optionService;
+
+        public SQuestionsController(ISQuestionService _squestionService,
+            IQuestionService _questionService, IOptionService _optionService)
         {
-            return new string[] { "value1", "value2" };
+            this._optionService = _optionService;
+            this._questionService = _questionService;
+            this._squestionService = _squestionService;
+        }
+        public IEnumerable<SQuestion> Get()
+        {
+            return _squestionService.GetAll();
         }
 
-        // GET: api/SQuestions/5
-        public string Get(int id)
+        [HttpGet]
+        [Route("{id:int}/questions")]
+        public IHttpActionResult Questions(int id)
         {
-            return "value";
+            var questions = _questionService.GetAll().Where(x => x.SQuestionID == id).ToList();
+            return Ok(questions);
+        }
+
+        [HttpGet]
+        [Route("{sqid:int}/questions/{id:int}/options")]
+        public IHttpActionResult Options(int sqid,int id)
+        {
+            var options = _optionService.GetAll().Where(x => x.QuestionID == id).ToList();
+            return Ok(options);
+        }
+        // GET: api/SQuestions/5
+        public async Task<SQuestion> Get(int id)
+        {
+            return await _squestionService.GetByIdAsync(id);
         }
 
         // POST: api/SQuestions
-        public void Post([FromBody]string value)
+        public async Task<IHttpActionResult> Post([FromBody]SQuestionViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var squestion = Mapper.Map<SQuestion>(model);
+                await _squestionService.CreateAsync(squestion);
+                return Ok();
+            }
+            return BadRequest(ModelState);
         }
 
         // PUT: api/SQuestions/5
