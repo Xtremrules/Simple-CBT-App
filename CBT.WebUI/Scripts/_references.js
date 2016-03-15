8c151deac3b1501a7bfb8f37658793f64e14a8c4
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-15 10:42:22 +0100

    Added services

--- CBT.WebUI/Scripts/_references.js
+++ CBT.WebUI/Scripts/_references.js
@@ -4,5 +4,6 @@
 /// <reference path="cbtapp/controller/questionsctrl.js" />
 /// <reference path="cbtapp/controller/rootctrl.js" />
 /// <reference path="cbtapp/controller/settingsctrl.js" />
+/// <reference path="cbtapp/services/appservices.js" />
 /// <reference path="Resources/angular-resource.js" />
 /// <reference path="Resources/angular-route.js" />
