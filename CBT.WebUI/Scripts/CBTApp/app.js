69afecaaf9d580fbb46e403b08661e4f15bad4a8
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-15 10:40:39 +0100

    Adsq modification in progress

--- CBT.WebUI/Scripts/CBTApp/app.js
+++ CBT.WebUI/Scripts/CBTApp/app.js
@@ -1,6 +1,5 @@
 ﻿angular.module("CBTApp", ["ngRoute"])
 .config(function ($routeProvider) {
-    //$routeProvider.otherwise({ templateUrl: "/Scripts/CBTApp/home/index.html" });
     $routeProvider.when("/batches", {
         templateUrl: "/Scripts/CBTApp/View/index.html", controller: "rootCtrl"
     })
@@ -9,6 +8,9 @@
     })
     .when("/questions", {
         templateUrl: "/Scripts/CBTApp/View/questions.html", controller: "questionsCtrl"
+    })
+    .when("/questions/add", {
+        templateUrl: "/Scripts/CBTApp/View/addSq.html", controller: "questionsCtrl"
     })
     .when("/settings/add", {
         templateUrl: "Scripts/CBTApp/View/addSettings.html", controller: "settingsCtrl"
