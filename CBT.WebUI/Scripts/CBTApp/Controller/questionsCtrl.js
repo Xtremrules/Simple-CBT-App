bbb4b25ed003eb28bf0d384e5407ff11d17908b2
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-15 10:02:59 +0100

    Added html content to view

--- CBT.WebUI/Scripts/CBTApp/Controller/questionsCtrl.js
+++ CBT.WebUI/Scripts/CBTApp/Controller/questionsCtrl.js
@@ -1,3 +1,13 @@
 ﻿angular.module("CBTApp")
-.controller("questionsCtrl", function ($scope, $http) {
+    .constant("SQuestions", "api/squestions")
+.controller("questionsCtrl", function ($scope, $http, SQuestions) {
+    $scope.data.empty = null;
+    $http.get(SQuestions).success(function (data) {
+        if (data.length <= 0) {
+            $scope.data.empty = true;
+        }
+        else {
+            $scope.data.SQuestions = data;
+        }
+    })
 });
