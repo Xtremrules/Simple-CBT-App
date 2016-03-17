eda66492a80d92d2c0bf8bad8855f1298e6a5746
Xtremrules <olelewechukwunyere@outlook.com>
2016-03-17 04:20:19 +0100

    Removed API Error

--- CBT.Domain/Services/SQuestionService.cs
+++ CBT.Domain/Services/SQuestionService.cs
@@ -1,11 +1,21 @@
 ﻿using CBT.Domain.Abstracts.Services;
 using CBT.Domain.Concrete;
 using CBT.Domain.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
 namespace CBT.Domain.Services
 {
     class SQuestionService : EntityService<SQuestion>, ISQuestionService
     {
         public SQuestionService(CBTDbContext context) : base(context) { }
+
+        public override IEnumerable<SQuestion> GetAll()
+        {
+            var d = _dbset.Include(x => x.Questions)
+                .Include(x => x.Setting).ToList();
+            return d;
+        }
     }
 }
